8046a7aa6cd20f0b1dd0452b6ae987f5da410d0b
jolenezheng <jolenezheng18@gmail.com>
2020-03-15 21:52:45 -0400

    scraped for article titles and url

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 import requests
 from bs4 import BeautifulSoup 
 
-BASE_URL = "https://ca.finance.yahoo.com/quote/"
+BASE_URL = "https://ca.finance.yahoo.com"
+PRICE_URL = "https://ca.finance.yahoo.com/quote/"
 
-my_tickers = ['QUIS.V', 'TD.TO', 'AAPL', 'XOM', 'WFC', 'GOOG']
+my_tickers = ['QUIS.V', 'TD.TO', 'XOM', 'WFC', 'AAPL', 'GOOG']
 
 for ticker in my_tickers:
-  URL = BASE_URL + ticker
+  URL = PRICE_URL + ticker + '/news'
   r = requests.get(URL) 
 
   soup = BeautifulSoup(r.content, 'html5lib') 
@@ -14,3 +15,19 @@ for ticker in my_tickers:
   price_header = soup.find('div', attrs = {'id':'quote-header-info'})
   price = price_header.find_all('span')[1].text
   print(ticker + "'s current price: " + price)
+
+  news_container = soup.find('ul', attrs = {'data-reactid':'3'})
+  articles = news_container.findChildren('li', recursive=False)
+  print('Articles for ' + ticker)
+
+  for article in articles[:3]:
+    article_url = article.find('div').find('div').findChildren('div')[2].findChildren('a', href=True)[0]['href']
+
+    r = requests.get(BASE_URL + article_url)
+    soup = BeautifulSoup(r.content, 'html5lib') 
+
+    article_title = soup.find('div', attrs = {'id':'YDC-Side-StackCompositeSideTop'}).find_all('h1')[0].text
+    print article_title
+
+  print('\n---\n')
+  
